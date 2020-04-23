ee9f7efbeecc12a48d48e82820588922d83a78cf
Evgeniya Olta <zhenechka.bodak@mail.ru>
2020-04-23 10:24:06 +0300

    cardarray

--- script.js
+++ script.js
@@ -8,6 +8,48 @@ let formElement = body.querySelector('.popup__container');
 let nameInput = popup.querySelector('[name="name"]');
 let descriptionInput = popup.querySelector('[name="description"]');
 
+
+const сards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+сards.forEach(function (item) {
+    const elementTemplate = document.querySelector('#card').content;
+    
+    const elements = document.querySelector('.elements');
+    const element = elementTemplate.cloneNode(true);
+    
+    element.querySelector('.element__image').src = item.link;
+    element.querySelector('.element__name').textContent = item.name;
+    
+    elements.append(element);
+});
+
+
+
 function openPopup() {
     popup.classList.add('popup_opened');
     popup.classList.remove('popup_closed');
